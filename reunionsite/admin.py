cad9ce04764367004c084ba6240ac4a65643b1a0
Ryan Hornik <ryan.hornik@towbee.com>
2016-03-14 12:55:26 -0500

    Created models and registered with admin. Marriages, names, and email addresses can now be modeled

--- reunionsite/admin.py
+++ reunionsite/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from reunionsite.models import Person, Email, Marriage
+
+
+class PersonAdmin(admin.ModelAdmin):
+    fields = ('first_name', 'last_name', 'email_addresses', 'spouse')
+    readonly_fields = ('email_addresses', 'spouse')
+    list_display = ('first_name', 'last_name')
+
+    def email_addresses(self, obj):
+        html = '<table>' \
+               '<thead><tr><th>Address</th></tr></thead>' \
+               '<tbody>'
+        for e in obj.email_addresses.all():
+            html += format_html('<tr><td><a href="/admin/reunionsite/email/{}/change">{}</a></td></tr>',
+                                e.pk, e.email_address)
+        html += '</tbody></table>'
+        return format_html(html)
+
+
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('email_address', 'owner')
+    list_filter = ('owner',)
+    search_fields = ('owner__first_name', 'owner__last_name')
+
+
+class MarriageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'spouse_a', 'spouse_b', 'status')
+
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Email, EmailAdmin)
+admin.site.register(Marriage, MarriageAdmin)
--- reunionsite/models.py
+++ reunionsite/models.py
@@ -1,1 +1,48 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Person(models.Model):
+    first_name = models.CharField(null=False, blank=False, max_length=255)
+    last_name = models.CharField(null=False, blank=False, max_length=255)
+
+    @property
+    def spouse(self):
+        try:
+            marriage = Marriage.objects.get(spouse_a=self, status=Marriage.MARRIED)
+            return marriage.spouse_b
+        except Marriage.DoesNotExist:
+            pass
+        try:
+            marriage = Marriage.objects.get(spouse_b=self, status=Marriage.MARRIED)
+            return marriage.spouse_a
+        except Marriage.DoesNotExist:
+            pass
+        return None
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Email(models.Model):
+    owner = models.ForeignKey(Person, null=False, related_name="email_addresses")
+    email_address = models.EmailField(null=False, blank=False)
+
+    def __str__(self):
+        return self.email_address
+
+
+class Marriage(models.Model):
+    MARRIED = 'M'
+    DIVORCED = 'D'
+    WIDOWED = 'W'
+    MARITAL_STATUS = (
+        (MARRIED, 'Married'),
+        (DIVORCED, 'Divorced'),
+        (WIDOWED, 'Widowed'),
+    )
+
+    spouse_a = models.ForeignKey(Person, null=False, related_name="spouse_a")
+    spouse_b = models.ForeignKey(Person, null=False, related_name="spouse_b")
+    status = models.CharField(max_length=1, choices=MARITAL_STATUS, default=MARRIED)
